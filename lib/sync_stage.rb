4c43e28f77d555a91d4caf7dd3e8f9d11e9390c9
Rene Paulokat <rene@so36.net>
2015-01-10 19:19:46 +0100

    wip

--- lib/sync_stage.rb
+++ lib/sync_stage.rb
@@ -2,89 +2,89 @@
 
 require 'yaml'
 
-class SyncStage
+module SyncStage
+  class Worker
 
-  def self.say_hi
-    yml = YAML::load_file(File.join(File.dirname(__FILE__), 'config', 'sync_stage.yml'))
-    puts "hi from sync_stage"
-    puts "#{yml.inspect}"
-  end
+    def self.say_hi
+      puts "hi from sync_stage"
+    end
 
-  def initialize(dir, init=nil)
-    puts "changing to " << dir
-    Dir.chdir(dir) do
-      if !File.exist?('sync_stage.yml')
-        if ! init
-          raise "Directory not initialized - try argument --init or see --help"
+    def initialize(dir, init=nil)
+      Dir.chdir(dir) do
+        if !File.exist?('sync_stage.yml')
+          if ! init
+            raise "Directory not initialized - try argument --init or see --help"
+          end
+          if init
+            puts "creating example config"
+            create_example_config(dir)
+          end
+        else
+          puts "trying existing config"
+          read_config
         end
-        if init
-          puts "creating example config"
-          create_example_config(dir)
-        end
-      else
-        puts "trying existing config"
-        read_config
       end
     end
-  end
 
-  def print_config
-    puts YAML::dump(@config)
-  end
+    def print_config
+      puts YAML::dump(@config)
+    end
 
-  def init_config
+    def init_config
 
-  end
+    end
 
-  def pg_dump_cmd(opts)
-    str = "pg_dump -U #{opts.fetch(:dbuser)} -h localhost -Fc #{opts.fetch(:db)} -f ~/#{opts.fetch(:db)}.sql"
-    puts "running #{str}"
-    str
-  end
+    def pg_dump_cmd(opts)
+      str = "pg_dump -U #{opts.fetch(:dbuser)} -h localhost -Fc #{opts.fetch(:db)} -f ~/#{opts.fetch(:db)}.sql"
+      puts "running #{str}"
+      str
+    end
 
-  def pack_assets_cmd(opts)
-    str = "cd #{opts.fetch(:shared_dir)} && tar cvfzp ~/#{opts.fetch(:host)}-shared.tgz ./public"
-    puts "running: " << str
-    str
-  end
+    def pack_assets_cmd(opts)
+      str = "cd #{opts.fetch(:shared_dir)} && tar cvfzp ~/#{opts.fetch(:host)}-shared.tgz ./public"
+      puts "running: " << str
+      str
+    end
 
-  def ssh_cmd(opts)
-    "ssh -l #{opts.fetch(:user)} #{opts.fetch(:host)} "
-  end
+    def ssh_cmd(opts)
+      "ssh -l #{opts.fetch(:user)} #{opts.fetch(:host)} "
+    end
 
-  def copy_db_cmd(from_opts, to_opts)
-    str = "scp #{from_opts.fetch(:user)}@#{from_opts.fetch(:host)}:~/#{from_opts.fetch(:db)}.sql "
-    str << " #{to_opts.fetch(:user)}@#{to_opts.fetch(:host)}:~/"
-    puts "running: #{str}"
-    str
-  end
+    def copy_db_cmd(from_opts, to_opts)
+      str = "scp #{from_opts.fetch(:user)}@#{from_opts.fetch(:host)}:~/#{from_opts.fetch(:db)}.sql "
+      str << " #{to_opts.fetch(:user)}@#{to_opts.fetch(:host)}:~/"
+      puts "running: #{str}"
+      str
+    end
 
-  def copy_assets_cmd(from_opts, to_opts)
-    str = "scp #{from_opts.fetch(:user)}@#{from_opts.fetch(:host)}:~/#{from_opts.fetch(:host)}-shared.tgz "
-    str << " #{to_opts.fetch(:user)}@#{to_opts.fetch(:host)}:#{to_opts.fetch(:shared_dir)}/"
-    puts "running " << str
-    str
-  end
+    def copy_assets_cmd(from_opts, to_opts)
+      str = "scp #{from_opts.fetch(:user)}@#{from_opts.fetch(:host)}:~/#{from_opts.fetch(:host)}-shared.tgz "
+      str << " #{to_opts.fetch(:user)}@#{to_opts.fetch(:host)}:#{to_opts.fetch(:shared_dir)}/"
+      puts "running " << str
+      str
+    end
 
-  def pg_restore_cmd(src, dest)
-    str = "sudo -u postgres pg_restore -U postgres -v -d #{dest.fetch(:db)} -c #{src.fetch(:db)}.sql"
-    puts str
-    str
-  end
+    def pg_restore_cmd(src, dest)
+      str = "sudo -u postgres pg_restore -U postgres -v -d #{dest.fetch(:db)} -c #{src.fetch(:db)}.sql"
+      puts str
+      str
+    end
 
-  private 
+    private 
 
-  def create_example_config(dir)
-    puts Dir.pwd
-    _example = YAML::load_file(File.join(File.dirname(__FILE__),'config', 'sync_stage.yml'))
-    File.open('sync_stage.yml', 'w') do |f|
-      f.puts YAML::dump(_example)
+    def create_example_config(dir)
+      puts Dir.pwd
+      _example = YAML::load_file(File.join(File.dirname(__FILE__),'config', 'sync_stage.yml'))
+      File.open('sync_stage.yml', 'w') do |f|
+        f.puts YAML::dump(_example)
+      end
+    end
+
+    def read_config
+      @config = YAML::load_file('sync_stage.yml')
     end
   end
 
-  def read_config
-    @config = YAML::load_file('sync_stage.yml')
-  end
 end
 
 
