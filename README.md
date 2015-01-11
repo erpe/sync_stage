8cf28eeefcf369ec529d72576b990d837517c371
Rene Paulokat <rene@so36.net>
2015-01-11 19:11:17 +0100

    works

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
 # sync_stage
-syncs back rails production data to staging
+Syncs back your Rails-app based production data to staging (or anywhere and vice versa)
 
-status: in progress... not functional
+
+The usual Rails deployment is done via capistrano and a specific deployment user.
+
+This helper enables you to sync your typical Rails-App between given staging-servers.
+
+## Example:
+
+```
+sync_stage -r 
+```
+dumps your database and packs your shared/public and copies it over to the destination host
+
+
+```
+sync_stage -r --restore
+```
+
+dumps database, packs shared/public, copies it to destination, 
+restores postgres dump, and unpacks shared/public to destinations shared/public
+restoring is done by `sudo -u postgres`
+
+```
+sync_stage -r --restores --explain
+```
+
+prints out what would happen, but not actually executes anything.
+
+
+
+## Prerequisites
+
+* expects your app to be deployed via capistrano
+* expects only postgres database
+* expects you have a deployment user
+* expects you're deployment user is a sudoer
+* expects your postgres password is available via .pgpass (see:  http://www.postgresql.org/docs/9.3/static/libpq-pgpass.html )
+
+
--- lib/sync_stage.rb
+++ lib/sync_stage.rb
@@ -1,48 +1,137 @@
 #!/usr/bin/env ruby
 
 require 'yaml'
+require 'pp'
 
 module SyncStage
   class Worker
 
-    def self.say_hi
-      puts "hi from sync_stage"
-    end
-
-    def initialize(dir, init=nil)
-      Dir.chdir(dir) do
+    def initialize(cfg)
+      s = Time.now
+      Dir.chdir(cfg.app) do
         if !File.exist?('sync_stage.yml')
-          if ! init
-            raise "Directory not initialized - try argument --init or see --help"
+          if ! cfg.init
+            puts "Directory not initialized - try argument --init or see --help"
+            exit 1
           end
-          if init
-            puts "creating example config"
-            create_example_config(dir)
+          if cfg.init
+            create_example_config(cfg.app)
           end
         else
-          puts "trying existing config"
           read_config
         end
       end
+      if cfg.explain
+        explain(cfg.restore)
+        exit 0
+      end
+      if cfg.run
+        run(cfg.restore)
+      end
+      duration = Time.now - s
+      puts "synced data \nfrom #{@config[:source].fetch(:host)} to #{@config[:destination].fetch(:host)} \n
+      done in #{duration.ceil} seconds."
     end
 
-    def print_config
-      puts YAML::dump(@config)
+    private 
+
+    def explain(restore)
+      puts "sync_stage would use following settings: \n\n"
+      pp @config
+
+      cmds = []
+      cmds.push(ssh_cmd(@config[:source]) << "'" << pg_dump_cmd(@config[:source]) << "'")
+      cmds.push(ssh_cmd(@config[:source]) << "'" << pack_assets_cmd(@config[:source]) << "'")
+      cmds.push(copy_db_cmd(@config[:source], @config[:destination]))
+      cmds.push(copy_assets_cmd(@config[:source], @config[:destination]))
+      if restore
+        cmds.push(ssh_cmd(@config[:destination]) << "-t '" << 
+                  pg_restore_cmd(@config[:source], @config[:destination]) << "'")
+        cmds.push(ssh_cmd(@config[:destination]) << "'" << 
+                  unpack_assets_cmd(@config[:source], @config[:destination]) << "'")
+      end
+      puts "\nand run the following commands in order: \n\n"
+      cmds.each_with_index do |c,i|
+        puts "#{i} $: " << c   
+      end
     end
 
-    def init_config
 
+    def run(restore)
+      puts "this could take a while....\n"
+      execute_db_dump
+      execute_asset_packing
+      execute_db_copying
+      execute_asset_copying
+      if restore
+        execute_db_restoring
+        execute_asset_unpacking
+      end
+    end
+
+    def execute_db_dump
+      ret = system(ssh_cmd(@config[:source]) << "'" << pg_dump_cmd(@config[:source]) << "'")
+      if ret
+        puts "done dumping database"
+      else
+        raise "error dumping database"
+      end
+    end
+
+    def execute_asset_packing
+      ret = system(ssh_cmd(@config[:source]) << "'" << pack_assets_cmd(@config[:source]) << "'")
+      if ret
+        puts "done packing shared directory"
+      else
+        raise "error packing shared directory"
+      end
+    end
+
+    def execute_db_copying
+      ret = system(copy_db_cmd(@config[:source], @config[:destination]))
+      if ret 
+        puts "done copying database over"
+      else
+        raise "error copying database over"
+      end
+    end
+
+    def execute_asset_copying
+      puts "start to copy over shared dir... be patient..."
+      ret = system(copy_assets_cmd(@config[:source], @config[:destination]))
+      if ret 
+        puts "done copying assets"
+      else
+        raise "error copying assets over"
+      end
+    end
+
+    def execute_db_restoring
+      ret = system(ssh_cmd(@config[:destination]) << " -t '" << 
+                  pg_restore_cmd(@config[:source], @config[:destination]) << "'")
+      if ret
+        puts "done restoring database"
+      else
+        raise "error restoring database"
+      end
+    end
+
+    def execute_asset_unpacking
+      ret = system(ssh_cmd(@config[:destination]) << "'" << unpack_assets_cmd(@config[:source], @config[:destination]) << "'")
+      if ret
+        puts "done unpacking shared dir"
+      else
+        raise "error unpacking shared dir"
+      end
     end
 
     def pg_dump_cmd(opts)
       str = "pg_dump -U #{opts.fetch(:dbuser)} -h localhost -Fc #{opts.fetch(:db)} -f ~/#{opts.fetch(:db)}.sql"
-      puts "running #{str}"
       str
     end
 
     def pack_assets_cmd(opts)
       str = "cd #{opts.fetch(:shared_dir)} && tar cvfzp ~/#{opts.fetch(:host)}-shared.tgz ./public"
-      puts "running: " << str
       str
     end
 
@@ -53,24 +142,26 @@ module SyncStage
     def copy_db_cmd(from_opts, to_opts)
       str = "scp #{from_opts.fetch(:user)}@#{from_opts.fetch(:host)}:~/#{from_opts.fetch(:db)}.sql "
       str << " #{to_opts.fetch(:user)}@#{to_opts.fetch(:host)}:~/"
-      puts "running: #{str}"
       str
     end
 
     def copy_assets_cmd(from_opts, to_opts)
       str = "scp #{from_opts.fetch(:user)}@#{from_opts.fetch(:host)}:~/#{from_opts.fetch(:host)}-shared.tgz "
       str << " #{to_opts.fetch(:user)}@#{to_opts.fetch(:host)}:#{to_opts.fetch(:shared_dir)}/"
-      puts "running " << str
       str
     end
 
     def pg_restore_cmd(src, dest)
       str = "sudo -u postgres pg_restore -U postgres -v -d #{dest.fetch(:db)} -c #{src.fetch(:db)}.sql"
-      puts str
       str
     end
 
-    private 
+    def unpack_assets_cmd(src, dest)
+      str = "cd #{dest.fetch(:shared_dir)} && 
+              mv public public-#{Time.now.to_i} && 
+              tar xvfzp #{src.fetch(:host)}-shared.tgz"
+      str
+    end
 
     def create_example_config(dir)
       puts Dir.pwd
@@ -84,32 +175,20 @@ module SyncStage
       @config = YAML::load_file('sync_stage.yml')
     end
   end
-
 end
 
 
 # pack the assets
 #ret = system(ssh_cmd(PRODUCTION) << "'" << pack_assets_cmd(PRODUCTION) << "'")
-#process = $?
-#puts "returned: #{ret} for #{process.inspect}"
-# copy the assets to other host
 #ret = system(copy_assets_cmd(PRODUCTION, STAGING))
-#process = $?
-#puts "returned: #{ret} for #{process.inspect}"
 
 # dump the database
 #ret = system(ssh_cmd(PRODUCTION) << "'" << pg_dump_cmd(PRODUCTION) << "'")
-#process = $?
-#puts "returned: #{ret} for #{process.inspect}"
 
 # copy db-dump to other host
 #ret = system(copy_db_cmd(PRODUCTION, STAGING))
-#process = $?
-#puts "returned: #{ret} for #{process.inspect}"
 
 # restore db from dump
 #puts "you need to be a sudoer"
 #ret = system(ssh_cmd(STAGING) << " -t '" << pg_restore_cmd(PRODUCTION, STAGING) << "'")
-#process = $?
-#puts "returned: #{ret} for #{process.inspect}"
 
